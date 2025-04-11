a176331f3d5a16ea9fc44474fc2b87ab83d7a423
keithlin724 <keithlin724@gmail.com>
2025-04-11 17:53:05 +0800

    remap dataset

--- data/MotionDataDescription.py
+++ data/MotionDataDescription.py
@@ -3,6 +3,11 @@ from pathlib import Path
 from dataclasses import dataclass
 from natsort import natsorted
 
+DEFAULT_CSV = {
+    "train": "Training_set.csv",
+    "test": "Testing_set.csv",
+}
+
 
 @dataclass(slots=True)
 class MotionDataDescription:
@@ -10,10 +15,9 @@ class MotionDataDescription:
     test_data: list[str]
     label: list[str]
     label_value: dict[str, int]
-    train_data_plus: pd.DataFrame = None
 
     @staticmethod
-    def build_from_folder(folder: str, with_plus: bool = False):
+    def build_from_folder(folder: str):
         folder_name = folder
         folder: Path = Path(folder_name)
 
@@ -21,23 +25,13 @@ class MotionDataDescription:
             raise FileNotFoundError(f"Folder {folder} does not exist")
 
         # preprocess the train data
-        csv_file = list(folder.glob("*.csv"))[0]
-        train_df = pd.read_csv(csv_file)
+        train_df = pd.read_csv(DEFAULT_CSV["train"])
 
-        mapping_path = lambda x: folder / "train_images" / x
+        mapping_path = lambda x: folder / "train" / x
         train_df["filename"] = train_df["filename"].apply(mapping_path)
 
-        if with_plus:
-            folder_plus = Path(f"{folder_name}_plus")
-            csv_file_plus = list(folder_plus.glob("*.csv"))[0]
-            train_df_plus = pd.read_csv(csv_file_plus)
-            mapping_path = lambda x: folder_plus / "train_images" / x
-            train_df_plus["filename"] = train_df_plus["filename"].apply(mapping_path)
-        else:
-            train_df_plus = None
-
         # list the test data
-        test_data = (folder / "test_images").glob("*.jpg")
+        test_data = (folder / "test").glob("*.jpg")
         test_data = natsorted(list(test_data))
 
         label = natsorted(train_df["label"].unique())
@@ -49,7 +43,6 @@ class MotionDataDescription:
             test_data=test_data,
             label=label,
             label_value=label_dict,
-            train_data_plus=train_df_plus,
         )
 
     def get_train_data_by_label(self, label: str):
@@ -83,8 +76,5 @@ class MotionDataDescription:
 
         val_data = self.train_data.loc[val_indices]
         train_data = self.train_data.drop(val_indices)
-
-        if self.train_data_plus is not None:
-            train_data = self.train_data_plus
 
         return train_data, val_data
