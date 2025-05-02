c8dd706c79ac5697150e0a6e37c2d70cbfbe36f7
Josh_61625 <leo900912forever60702@gmail.com>
2025-05-02 13:43:04 +0800

    rename_file

---
+++ train_model_cnn.py
@@ -0,0 +1,67 @@
+# %%
+import os
+
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+import lightning as L
+from lightning.pytorch.loggers import TensorBoardLogger
+from lightning.pytorch.callbacks import ModelCheckpoint, EarlyStopping
+
+from model import CNNHARModel
+from model import ClipHARModel
+from data import MotionDataDescription, MotionDataModule
+
+import torch
+
+torch.set_float32_matmul_precision("high")
+
+
+EPOCH = 10
+
+# %%
+data_decs = MotionDataDescription.build_from_folder("./Human Action Recognition")
+
+# %%
+
+datamodule = MotionDataModule(data_decs, batch_size=32, val_size=0.2)
+# %%
+model = CNNHARModel(labels=data_decs.label)
+# model = ClipHARModel(labels=data_decs.label)
+print(model)
+
+# %%
+# print(model)
+# %%
+tb_logger = TensorBoardLogger("logs", name="cnn_har")
+
+checkpoint_callback = ModelCheckpoint(
+    monitor="val_loss",
+    # dirpath="checkpoints",
+    filename="model-{epoch:02d}-{val_loss:.2f}",
+    save_top_k=3,
+    mode="min",
+    save_last=True,
+)
+
+
+early_stop_callback = EarlyStopping(
+    monitor="val_loss",
+    patience=10,
+    mode="min",
+)
+
+# %%
+trainer = L.Trainer(
+    accelerator="gpu",
+    devices=1,
+    callbacks=[checkpoint_callback, early_stop_callback],
+    logger=tb_logger,
+    max_epochs=EPOCH,
+    log_every_n_steps=20,
+    fast_dev_run=True,
+)
+
+# %%
+trainer.fit(model, datamodule=datamodule)
+
+# %%
