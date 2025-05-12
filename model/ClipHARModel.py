2b45c7c0fa4f30e334eb9565ad1a3a52a1fd159e
Josh_61625 <leo900912forever60702@gmail.com>
2025-05-12 19:10:34 +0800

    處理 git pull-4

--- model/ClipHARModel.py
+++ model/ClipHARModel.py
@@ -106,20 +106,20 @@ class ClipHARModel(L.LightningModule):
         return out
 
     def configure_optimizers(self):
-        # optimizer = optim.AdamW(self.parameters(), lr=1e-3, weight_decay=1e-2)
-        # total_steps = 100000
+        optimizer = optim.AdamW(self.parameters(), lr=1e-3, weight_decay=1e-2)
+        total_steps = 100000
 
-        # scheduler = CosineAnnealingLR(optimizer, T_max=total_steps, eta_min=1e-6)
+        scheduler = CosineAnnealingLR(optimizer, T_max=total_steps, eta_min=1e-6)
 
-        # return {
-        #     "optimizer": optimizer,
-        #     "lr_scheduler": {
-        #         "scheduler": scheduler,
-        #         "interval": "step",
-        #     },
-        # }
-        optimizer = DAdaptAdam(self.parameters())
-        return optimizer
+        return {
+            "optimizer": optimizer,
+            "lr_scheduler": {
+                "scheduler": scheduler,
+                "interval": "step",
+            },
+        }
+        # optimizer = DAdaptAdam(self.parameters())
+        # return optimizer
 
     def training_step(self, batch, batch_idx):
         x, y = batch
